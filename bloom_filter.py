0bfbc2de94c5b6ec9d420eb72a82d2160d3028dc
Ragbandir Hanush Singh <148356740+hanushrajputh@users.noreply.github.com>
2024-11-04 11:36:32 +0530

    Update bloom_filter.py

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,4 +1,4 @@
-
+# bloom_filter.py
 import mmh3
 from bitarray import bitarray
 
@@ -10,14 +10,17 @@ class BloomFilter:
         self.bit_array.setall(0)
 
     def add(self, item):
+        # Convert item to a string before hashing
+        item = str(item)
         for i in range(self.hash_count):
             digest = mmh3.hash(item, i) % self.size
             self.bit_array[digest] = 1
 
     def check(self, item):
+        # Convert item to a string before hashing
+        item = str(item)
         for i in range(self.hash_count):
             digest = mmh3.hash(item, i) % self.size
             if self.bit_array[digest] == 0:
                 return False
         return True
-    
